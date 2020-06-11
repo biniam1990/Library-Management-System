8b02c599b7980f5e6c79a85a5a921ea2a3a18ae8
biniam.shibru@toyota.com <biniam.shibru@toyota.com>
2020-06-11 11:02:14 -0500

    added controller

---
+++ library-management/src/main/java/com/management/library/librarymanagement/controller/BookController.java
@@ -0,0 +1,17 @@
+package com.management.library.librarymanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+	@GetMapping("/title")
+	public String getBooktitle() {
+		
+		return "title";
+	}
+	
+}
