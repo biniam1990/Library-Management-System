194b25221ddc9a8ebc83dc9a026a414a835b14d5
biniam.shibru@toyota.com <biniam.shibru@toyota.com>
2020-08-26 01:38:38 -0500

    test cases

--- library-management/src/test/java/com/management/library/librarymanagement/controller/LibraryControllerTests.java
+++ library-management/src/test/java/com/management/library/librarymanagement/controller/LibraryControllerTests.java
@@ -1,12 +1,14 @@
 package com.management.library.librarymanagement.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -18,8 +20,9 @@ public class LibraryControllerTests {
 	
 	@Test
 	public void testGetBooktitle() throws Exception {
-		this.mockMvc.perform(get("/books/title"))
-		            .andExpect(status().is2xxSuccessful());
+		this.mockMvc.perform(get("/books/title").accept(MediaType.APPLICATION_JSON))
+		            .andExpect(status().is2xxSuccessful())
+		            .andExpect(content().string("title"));
 		
 	}
 
